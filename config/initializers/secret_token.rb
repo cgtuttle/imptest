d4141fdb8894170309ee2e74893f598a57dc28a0
Carl Tuttle <cgtuttle@gmail.com>
2013-05-22 12:51:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imptest::Application.config.secret_token = '826421ec9eadb8783e56e43410759c1e179e51b317680eb553557481b89e8675897813eef0182ef310e48b5394fdc1093d134f5643f801bf567ba35bf4b8789f'
