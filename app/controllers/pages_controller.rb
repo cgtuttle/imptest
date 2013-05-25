a292d7e05c17dfa25be3de9ee5562bea494f2901
cgtuttle <cgtuttle@gmail.com>
2013-05-25 14:27:41 -0500

    added to def main

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
 
 	def main
+		@value = params[:value]
+		@channel = params[:channel]
+		@sample = Sample.new
+		@sample.value = @value
+		@sample.sensor_id = @channel
+		@sample.save
 	end
 
 end
