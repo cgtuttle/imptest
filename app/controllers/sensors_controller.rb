b3cd02a43d2dc21433f50a6768d870ff256927cd
cgtuttle <cgtuttle@gmail.com>
2013-05-25 16:27:51 -0500

    set up index to truncate data at 25 records

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -2,13 +2,17 @@ class SensorsController < ApplicationController
 
 	def push
 		@value = params[:value]
-		if @value > 0
+		if !(!@value || @value == 0)
 			@channel = params[:channel]
 			@sample = Sample.new
 			@sample.value = @value
 			@sample.sensor_id = @channel
 			@sample.save
 		end
+		while Sample.count > 25
+			Sample.first.delete
+		end
+		render :nothing => true
 	end
 
 	def index
