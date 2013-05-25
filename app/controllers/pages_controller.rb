2c8f5bd16e9818d634cb61927d8f46603c277e4e
cgtuttle <cgtuttle@gmail.com>
2013-05-25 15:01:17 -0500

    fixed index view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,6 @@
 class PagesController < ApplicationController
 
 	def main
-		@value = params[:value]
-		@channel = params[:channel]
-		@sample = Sample.new
-		@sample.value = @value
-		@sample.sensor_id = @channel
-		@sample.save
-		@samples = Sample.all
 	end
 
 end
